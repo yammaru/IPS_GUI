5bd144bb4d6f9ef8d0be33c96d4268070a303d1d
yammaru <63098405+yammaru@users.noreply.github.com>
2020-05-04 08:19:04 -0500

    Add files via upload

---
+++ BLL/LiquidacionCuotaModeradoraService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entity;
+
+namespace BLL
+
+{
+    public class LiquidacionCuotaModeradoraService
+    {
+        LiquidacionCuataMoeradoraRepository RepositorioLiquidacion = new LiquidacionCuataMoeradoraRepository();
+        public string Guardar(LiquidacionCuotaModeradora liquidacion)
+        {
+            if (RepositorioLiquidacion.Buscar(liquidacion.NumeroLiquidacion)==null)
+            {
+                RepositorioLiquidacion.Guardar(liquidacion);
+                return $"la liquidacion fue guarda";
+            }
+            return $"paciente {liquidacion.NumeroLiquidacion}, ya exite";
+        }
+
+       
+        public List<LiquidacionCuotaModeradora> Consultar() { return RepositorioLiquidacion.Consultar(); }
+        public List<LiquidacionCuotaModeradora> Filtro(string id,int mes,int año){return RepositorioLiquidacion.Filtro(id,mes,año);}
+        public LiquidacionCuotaModeradora Buscar(string id) { return RepositorioLiquidacion.Buscar(id); }
+        public decimal Totalizar() { return RepositorioLiquidacion.Totalizar(); }
+    }
+}
