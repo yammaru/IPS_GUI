2c5cca24dd3a68dde77ac9df7a517070fd21f3f8
yammaru <63098405+yammaru@users.noreply.github.com>
2020-05-04 08:18:04 -0500

    Add files via upload

---
+++ Entity/LiquidacionCuotaModeradora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public abstract class LiquidacionCuotaModeradora
+    {
+        public decimal Tarifa { get; set; }
+        public string NumeroLiquidacion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string IdentificacionPasiente { get; set; }
+        public string Tipo { get; set; }
+        public decimal SalarioPaciente { get; set; }
+        public decimal ValorServicio { get; set; }
+        public decimal CuotaModeradora { get; set; }
+        public LiquidacionCuotaModeradora(string numeroLiquidacion,DateTime fecha, string identificacionPaciente, string tipo, decimal salarioPaciente,decimal valorServicio)
+        {
+            NumeroLiquidacion = numeroLiquidacion;
+            Fecha = fecha;
+            IdentificacionPasiente = identificacionPaciente;
+            Tipo = tipo;
+            SalarioPaciente = salarioPaciente;
+            ValorServicio = valorServicio;
+        }
+        public LiquidacionCuotaModeradora()
+        {
+
+        }
+        public abstract void Liquidar();
+      
+        public override string ToString()
+        {
+            return $"{NumeroLiquidacion};{Fecha};{IdentificacionPasiente};{Tipo};{SalarioPaciente};{Tarifa};{ValorServicio};{CuotaModeradora}";
+        }
+    }
+}
---
+++ Entity/RegimenSubsidiado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+  public  class RegimenSubsidiado : LiquidacionCuotaModeradora
+    {
+        public RegimenSubsidiado()
+        {
+        }
+
+        public RegimenSubsidiado(string numeroLiquidacion, DateTime fecha, string identificacionPaciente, string tipo, decimal salarioPaciente, decimal valorServicio) : base(numeroLiquidacion, fecha, identificacionPaciente, tipo, salarioPaciente, valorServicio)
+        {
+        }
+
+        public override void Liquidar()
+        {        
+            Tarifa = 0.05m;
+            CuotaModeradora = ValorServicio * Tarifa;
+            if (CuotaModeradora >= 200000)
+            {
+                CuotaModeradora = 200000;
+            }            
+        }
+    }
+}
